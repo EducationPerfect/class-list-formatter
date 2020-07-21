534db5c184bc981d9e3be01f89d27c88e709d4c7
Arron Dick <doglitbug@gmail.com>
2020-07-21 16:42:36 +1200

    First Commit

---
+++ Config/config.ps1
@@ -0,0 +1,233 @@
+﻿<#
+.Synopsis
+Configuration file
+
+.Description
+v 2020.07.08
+Required files(inputs):
+-----------------------
+-files.rejectsFile
+	This file defines regular expressions to be applied to class names to reject by.
+	Rejected classes are usually non-academic classes. This is an exclusion list.
+	Any data that matches these Regular Expressions will be output to files.rejectsOutFile
+	This file will usually have been provided by EP staff based on existing provided data, but below is a base to create your own.
+	Please note that adding a regular expression to this list will not remove it from EP, it just stops it being sent/updated from your end.
+	___________________________________________
+	|regex             | Description          |
+	-------------------------------------------
+	|(STUDY)           | Remove study periods |
+	|([A-Z]{1}\d{1,2}) | Remove form classes  |
+	-------------------------------------------
+
+-files.classCodesFile
+	This file contains mappings of 3-4 letter class codes to human readable descriptions.
+	These mappings are required for licencing and categorizations. This is an inclusion-list.
+	Any data that doesn't match these codes will be output to files.unknownOutFile
+	This file will usually have been provided by EP staff based on existing provided data, but below is a base to create your own.
+	Modifying a class description will not change the name on EP, it will likely create a duplicate class instead. Please change the name in the Control Panel as well if this is required.
+	Please note that any duplicate codes, will result in only the first Description found being used.
+	
+	__________________________________
+	|Code      | Description         |
+	----------------------------------
+	|SCI       | Science             |
+	|ENG       | English             |
+	|EENG      | Essential English   |
+	|MAT       | Maths               |
+	----------------------------------
+
+	It is possible to add extra codes to the bottom of this file to have them included in the student upload file.
+	For example, if your school is also signing up for Languages, you could add the following:
+	|GER       | German              |
+	|FRE       | French              |
+	|SPA       | Spanish             |
+	|CHI       | Chinese             |
+	----------------------------------
+	Please note that the above are only suggestions and will depend on the particular naming conventions for class codes in your school.
+	Do not include the year level or the modifier/stream code(eg the class codes of 12SCIA, 11SCIB and 12SCIB, would only require SCI to be added to this file)
+ 
+-files.inputFile
+	This is the file to process, usually the export from your student management system
+	
+Optional File(outputs):
+-----------------------
+-files.studentOutFile
+	Where to output processed student data, this file is to be uploaded to '/students/' on EPs SFTP server
+	Use "" to suppress creation
+	
+-files.teacherOutFile
+	Where to output teacher data, this file is to be uploaded to '/teachers/' on EPs SFTP server
+	Use "" to suppress creation
+	
+-files.rejectsOutFile
+	Where to output rejected student data, please review this file to double check none of these classes should be in files.studentOutFile
+	Use "" to suppress creation
+	
+-files.unknownOutFile
+	Where to output unknown student data, please review this file to double check none of these classes should be in files.studentOutFile
+	Any classes that are in this file that should have been in files.studentOutFile will need to be added to files.classCodes and process script re-run.
+	
+	For example, if 12SCIA and 11SCIB are in this file, please add the following to files.classCodes
+	|SCI       | Science             |
+	----------------------------------
+	Use "" to suppress creation
+  
+Teacher data:
+-------------
+-teachers.forename
+	Column name containing Teachers Forename. Please do not include titles eg Mr. This is a required column.
+	
+-teachers.surname
+	Column name containing Teachers Surname. Use "" if entire name is contained in teachers.forename. If there is only one field for a teachers full name, files.teachers will not be able to be generated.
+
+-teachers.email
+	Column name containing Teacher Email address. This is a required column.
+	
+-teachers.LTI
+	Column name containing Teachers LTI Identifier.
+	Can be the same as teacher.email if your LTI identifier is the same.
+	Use "" if your school does not use LTI Identifiers with EP
+	
+-teachers.SSO
+	Column name containing Teachers SSO Identifier.
+	Can be the same as teacher.email if your SSO identifier is the same.
+	Use "" if your school does not use SSO Identifiers with EP
+	
+Student data:
+-------------
+-students.forename
+	Column name containing Students Forename. This is a required column.
+	
+-students.surname
+	Column name containing Students Surname. This is a required column.
+
+-students.email
+	Column name containing Students Email address. This is a highly reccomended column.
+	
+-students.studentID
+	Column name containing Students ID. This is an optional column but highly reccommended, use "" if not required.
+	
+-students.LTI
+	Column name containing Students LTI Identifier.
+	Can be the same as students.email if your LTI identifier is the same.
+	Use "" if your school does not use LTI Identifiers with EP
+	
+-students.SSO
+	Column name containing Students SSO Identifier.
+	Can be the same as students.email if your SSO identifier is the same.
+	Use "" if your school does not use SSO Identifiers with EP
+
+-students.UID
+	Column name containing a unique identifier for each student in your school and *must* be provided on *every* row in your export.
+	This will usually be the same as -students.email column unless your school doesn't use email addresses or has partial coverage, in which case it will need to be something like the students.studentID column
+	This value is required as it is used to group classes that have more than one teacher. Mistyping this value will well break things.
+
+Class data:
+-----------
+-classes.prepend
+    Column name to be prepended to the start of the class name, used for EP schools that have more than one Campus
+    Use "" if the field is not required
+
+-classes.classCode
+	Column name containing unique class codes, eg 12SCIA. This is a required column
+	
+-classes.rexEx1
+-classes.regEx2
+-classes.regEx3
+	These field defines Regular Expressions to be applied to -classes.classCode
+	The default is "(?i)^(?'Year'\d+)(?'Code'[A-Z]{2,})(?'Modifier'\d*)$" which will match codes like the following
+	7ENG1
+	12CHE10
+	08BIO2
+	7MATH
+	
+	The named capture field "Code" is required at a minimum.
+	Capture fields are:
+	- Campus
+	- Year
+	- Modifier
+	- Code
+	
+    A final class name is generated on EP using the following formula:
+    [Current Year ][-regEx.Campus ][-classes.prepend ]Year [-regEx.Year][.-regEx.Modifier] [Class Description] ([Class Code]) - [Teacher/s Surnames]
+
+	Use "" if the regEx[2-3] fields are not required
+
+Additional functions:
+---------------------
+-PreProcess
+-PostProcess
+	If the above functions are provided in this file, they will run before and after processing is done in the main script.
+	Often these will be provided to perform additional tasks particular to your school, such as adding a term number to class names
+	
+	Not required, but please do not remove if provided
+	
+-showSummary
+	If set to true, a small summary of the processing will be output
+#>
+
+
+$global:config = @{
+
+	files = @{
+		rejectsFile = "Config\rejects.csv"
+		classCodesFile = "Config\classCodes.csv"
+		inputFile = "export.csv"
+		studentOutFile = "students.csv"
+		teacherOutFile = "teachers.csv"
+		rejectsOutFile = "rejects.csv"
+		unknownOutFile = "unknown.csv"
+	}
+	
+	teachers = @{
+		forename = "Teacher forename"
+		surname = "Teacher surname"
+		email = "Teachers email"
+		LTI = ""
+		SSO = ""
+		code = ""
+	}
+	
+	students = @{
+		forename = "FORENAME"
+		surname = "SURNAME"
+		studentID = "UPN Student"
+		email = "Email"
+		LTI = ""
+		SSO = ""
+		UID = "UPN Student"
+	}
+	
+	classes = @{
+        prepend = ""
+		classCode = "CLASS NAME"
+		regEx1 = "(?i)^(?'Year'\d+)(?'Code'[A-Z]{3,})(?'Modifier'\d+)$"
+		regEx2 = ""
+		regEx3 = ""
+	}
+	
+	yearLevels = @{
+	"X"  = "10"
+	"AE" = "11"
+	"AT" = "12"
+	
+	"L1" = "11"
+	"L2" = "12"
+	"L3" = "13"
+	"0001" = "00/01"
+	"0102" = "01/02"
+	"34" = "03/04"
+	"45" = "04/05"
+	"56" = "05/06"
+	"67" = "06/07"
+	"78" = "07/08"
+	"89" = "08/09"
+	"1011"="10/11"
+	"1112"="11/12"
+	"1213"="12/13"
+	
+	""   = "??"
+	}
+	
+	showSummary = $false
+}
---
+++ EPProcess.ps1
@@ -0,0 +1,351 @@
+﻿<#
+.Synopsis
+Reformatter for your schools data export.
+Will format your schools SMS export to 'students.csv' and 'teachers.csv' for upload to EPs SFTP server
+
+.Description
+v 2020.07.08
+This tool will reformat your schools data export in to a format that can be automatically processed by EP.
+
+The following file(s) are required:
+-Config\config.ps1
+ This file contains configuration data required by the tool and is contained in a seperate file as this data will be particular to your school.
+ See get-help .\Config\config.ps1 for more help
+
+.PARAMETER rejectsFile
+Override location and file name for rejects input file.
+
+.PARAMETER classCodesFile
+Override location and file name for classCodes input file.
+
+.PARAMETER inputFile
+Override location and file name for export input file.
+
+.PARAMETER studentOutFile
+Override location and file name for student output file. Use "" to suppress creation.
+
+.PARAMETER teacherOutFile
+Override location and file name for teacher output file. Use "" to suppress creation.
+
+.PARAMETER rejectsOutFile
+Override location and file name for rejects output file. Use "" to suppress creation.
+
+.PARAMETER unknownOutFile
+Override location and file name for unknown output file. Use "" to suppress creation.
+#>
+
+param(
+    [string]$rejectsFile,
+    [string]$classCodesFile,
+    [string]$inputFile,
+    [string]$studentOutFile,
+    [string]$teacherOutFile,
+    [string]$rejectsOutFile,
+    [string]$unknownOutFile
+    )
+
+Set-PSDebug -Strict
+
+#Load configuration parameters, not done before param() as it needs to run first
+. Config\config.ps1
+
+#Load defaults if not overridden
+#Input files
+if (!$PSBoundParameters.ContainsKey('rejectsFile')) {$rejectsFile = $global:config.files.rejectsFile}
+if (!$PSBoundParameters.ContainsKey('classCodesFile')) {$classCodesFile = $global:config.files.classCodesFile}
+if (!$PSBoundParameters.ContainsKey('inputFile')) {$inputFile = $global:config.files.inputFile}
+
+#Output files
+if (!$PSBoundParameters.ContainsKey('studentOutFile')) {$studentOutFile = $global:config.files.studentOutFile}
+if (!$PSBoundParameters.ContainsKey('teacherOutFile')) {$teacherOutFile = $global:config.files.teacherOutFile}
+if (!$PSBoundParameters.ContainsKey('rejectsOutFile')) {$rejectsOutFile = $global:config.files.rejectsOutFile}
+if (!$PSBoundParameters.ContainsKey('unknownOutFile')) {$unknownOutFile = $global:config.files.unknownOutFile}
+
+#Convert csv to utf8 due to bug in Import-CSV
+$tempFile = "utf8" + $inputFile
+Rename-Item -Path $inputFile -NewName $tempFile
+Get-Content $tempFile | Set-Content -Encoding utf8 $inputFile
+Remove-Item $tempFile
+
+#Run any require PreProcess from config
+if (Get-Command 'PreProcess' -ErrorAction SilentlyContinue){
+    PreProcess
+}
+
+#Import reject list and create regex to use on CLASS NAME column
+try {
+    $rejectFile = Import-CSV $rejectsFile | select -ExpandProperty regex -ErrorAction Stop
+} catch [System.IO.FileNotFoundException] {
+    "Error: $rejectsFile file not found"
+    exit
+}
+
+#Add key to account for empty file, or else everything gets rejected
+$rejectRegex = if($rejectFile.Count -gt 0){ $rejectFile -join "|"} else { "abc1234" }
+
+#Import class code data
+try {
+    $classCodes = Import-Csv $classCodesFile | group -AsHashTable -Property Code -ErrorAction Stop
+    if ($classCodes.Count -eq 0) {
+        "Error: Class Codes file $classCodesFile empty"
+        exit
+    }
+} catch [System.IO.FileNotFoundException] {
+    "Error: Class Codes File $classCodesFile not found"
+    exit
+}
+
+#Import spreadsheet data
+try {
+    $spreadsheet = Import-Csv $inputFile -ErrorAction Stop
+} catch [System.IO.FileNotFoundException] {
+    "Error: Input file $inputFile not found"
+    exit
+}
+
+#Extract teachers if requried/possible
+if ($teacherOutFile -ne "" -and $global:config.teachers.surname -ne ""){
+    #Do teachers
+    $teachers = $spreadsheet | Select -Property `
+    @{label="Forename";expression={$_.($global:config.teachers.forename)}},
+    @{label="Surname";expression={$_.($global:config.teachers.surname)}},
+    @{label="Email";expression={$_.($global:config.teachers.email).ToLower()}},
+    @{label="SSO Identifier";expression={$_.($global:config.teachers.SSO).ToLower()}},
+    @{label="LTI Identifier";expression={$_.($global:config.teachers.LTI).ToLower()}},
+    @{label="Teacher Code";expression={$_.($global:config.teachers.code).ToUpper()}} `
+    | Sort "Email" -Unique `
+    | Where-Object{$_.Email -and $_.Forename -and $_.Surname}
+    
+    #Do post processing that can't be done in the select
+    #SSO
+    if ($global:config.teachers.SSO -eq ""){
+        $teachers = $teachers | Select-Object -Property * -ExcludeProperty "SSO Identifier"
+    }
+
+    #LTI
+    if ($global:config.teachers.LTI -eq ""){
+        $teachers = $teachers | Select-Object -Property * -ExcludeProperty "LTI Identifier"
+    }
+
+    #Code
+    if ($global:config.teachers.code -eq ""){
+        $teachers = $teachers | Select-Object -Property * -ExcludeProperty "Teacher Code"
+    }
+    
+    #Finally save it
+    $teachers | Export-Csv -Path $global:config.files.teacherOutFile -Force -NoTypeInformation -Encoding utf8
+}
+
+#Join teachers forename and surname together if required else assume entire name
+if ($global:config.teachers.surname -ne ""){
+    $spreadsheet = $spreadsheet | Select-Object *, @{Name="Teacher/s";Expression={$_.($global:config.teachers.forename)+" "+$_.($global:config.teachers.surname)}}
+} else {
+    $spreadsheet = $spreadsheet | Select-Object *, @{Name="Teacher/s";Expression={$_.($global:config.teachers.forename)}}
+    #TODO Split name up depending on comma or first space or capitals?
+}
+
+#Join staff name columns and group by student/classname for classes with more than one teacher
+$spreadsheet = $spreadsheet | Group-Object -Property {$_.($global:config.students.UID) + $_.($global:config.classes.classCode)} 
+
+#Set up collections for output files: uploads, rejects and unknown codes
+$upload = @()
+$rejected = @()
+$unknown = @()
+
+#Generate a nicely formatted output row that should be able to be automatically proccessed.
+#Assume that class code matches at this point
+function Generate-OutputRow($matches, $row){
+    $campus = ""
+    $prepend = ""
+    $year = ""
+    $modifier = ""
+    $subject = ""
+    $code= ""
+
+
+    #Campus code
+    if($matches['Campus'].Length -gt 0){
+        $campus = $matches['Campus']+" ";
+    }
+
+    #Prepend field
+    if($global:config.classes.prepend.Length -gt 0){
+        $prepend = $row.Group[0].($global:config.classes.prepend)+" ";
+    }
+
+    #Year, look up code or convert to two digits
+    if($matches['Year'].Length -gt 0){
+	    if($global:config.yearLevels.($matches['Year']).Length -gt 0) {
+		    $year = "Year " + $global:config.yearLevels.($matches['Year'])
+	    } else {
+		    $year = "Year {0:d2}" -f [int32]($matches['Year'])
+	    }
+    }
+
+    #Modifier
+    if ($year.Length -gt 0){
+        if ($matches['Modifier'].Length -gt 0){
+            $modifier ="." + $matches['Modifier'] + " "
+        } else {
+            $modifier = " "
+        }
+    }
+
+    #Subject
+    $subject = $classCodes[$matches['Code']].Description
+
+    #code
+    $code = $matches[0]
+
+    #Generate class name
+    $classname = $campus + $prepend + $year + $modifier + $subject + " (" + $code + ")"
+
+    #Generate student row
+    $outputrow = [PSCustomObject]@{
+        Forename = $row.Group[0].($global:config.students.forename)
+        Surname = $row.Group[0].($global:config.students.surname)
+        Class = $classname
+        "Teacher/s" = $row.Group."Teacher/s" -Join ", "
+        "Student ID" = $row.Group[0].($global:config.students.studentID)
+        Email = $row.Group[0].($global:config.students.email)
+        "LTI Identifier" = $row.Group[0].($global:config.students.LTI)
+        "SSO Identifier" = $row.Group[0].($global:config.students.SSO)
+    }
+
+    #Do post processing that can't be done in the custom object
+    #Student ID
+    if ($global:config.students.studentID -eq ""){
+        $outputrow = $outputrow | Select-Object -Property * -ExcludeProperty "Student ID"
+    } 
+
+    #LTI
+    if ($global:config.students.LTI -eq ""){
+        $outputrow = $outputrow | Select-Object -Property * -ExcludeProperty "LTI Identifier"
+    }
+
+    #SSO
+    if ($global:config.students.SSO -eq ""){
+        $outputrow = $outputrow | Select-Object -Property * -ExcludeProperty "SSO Identifier"
+    }
+   
+    return $outputrow
+}
+
+#Loop through all row data
+Foreach ($row IN $spreadsheet)
+{
+
+#Cascading if statements here instead of a switch?
+    switch -regex ($row.Group.($global:config.classes.classCode))
+    {
+        #Ditch rejected class codes
+        $rejectRegex
+        {
+            $rejected += $row.Group
+            break
+        }
+
+        #Work on class codes that match expected format 1
+        $global:config.classes.regEx1
+        {            
+            if ($global:config.classes.regEx1 -ne ""){
+                #Do we have a matching code?
+                If ($classCodes.ContainsKey($matches['Code'])) {
+                    $upload += Generate-OutputRow $matches $row
+                } else {
+                    $unknown += $row.Group
+                }
+                break
+            }
+        }
+
+        #Work on class codes that match expected format 2
+        $global:config.classes.regEx2
+        {            
+            if ($global:config.classes.regEx2 -ne ""){
+                #Do we have a matching code?
+                If ($classCodes.ContainsKey($matches['Code'])) {
+                    $upload += Generate-OutputRow $matches $row
+                } else {
+                    $unknown += $row.Group
+                }
+                break
+            }
+        }
+
+        #Work on class codes that match expected format 3
+        $global:config.classes.regEx3
+        {            
+            if ($global:config.classes.regEx3 -ne ""){
+                #Do we have a matching code?
+                If ($classCodes.ContainsKey($matches['Code'])) {
+                    $upload += Generate-OutputRow $matches $row
+                } else {
+                    $unknown += $row.Group
+                }
+                break
+            }
+        }
+
+        #Anything else: Didn't match any RegEx pattern provided
+        ""
+        {
+            $unknown += $row.Group
+            break
+        }
+    }
+}
+
+#Export processed data
+if ($studentOutFile -ne "") {$upload | Export-Csv -Path $studentOutFile -NoTypeInformation -Force -Encoding utf8}
+
+#Export rejected data
+if ($rejectsOutFile -ne "") {$rejected | Export-Csv -Path $rejectsOutFile -NoTypeInformation -Force -Encoding utf8}
+
+#Export unknown class codes
+if ($unknownOutFile -ne "") {$unknown | Export-Csv -Path $unknownOutFile -NoTypeInformation -Force -Encoding utf8}
+
+if($global:config.showSummary){
+    Write-Output "Processing Report"
+    Write-Output "-----------------"
+
+    Write-Output "`nProcessed Rows:"
+    Write-Output "---------------"
+    $tmp_accepted = $upload.length
+    $tmp_rejected = $rejected.length
+    $tmp_unknown = $unknown.length
+    $tmp_total = ($tmp_accepted + $tmp_rejected + $tmp_unknown)
+    Write-Output "Accepted:`t $tmp_accepted"
+    Write-Output "Rejected:`t $tmp_rejected"
+    Write-Output "Unknown:`t $tmp_unknown"
+    Write-Output "Total:`t`t $tmp_total"
+
+
+    Write-Output "`nProcessed Classes:"
+    Write-Output "------------------"
+    $tmp_accepted = ($upload | sort-Object -Property "Class" -Unique).Count
+    $tmp_rejected = ($rejected | sort-Object -Property "CLASS NAME" -Unique).Count
+    $tmp_unknown = ($unknown | sort-Object -Property "CLASS NAME" -Unique).Count
+    $tmp_total = ($tmp_accepted + $tmp_rejected + $tmp_unknown)
+    Write-Output "Accepted:`t $tmp_accepted"
+    Write-Output "Rejected:`t $tmp_rejected"
+    Write-Output "Unknown:`t $tmp_unknown"
+    Write-Output "Total:`t`t $tmp_total"
+
+
+    Write-Output "`nProcessed Students:"
+    Write-Output "-------------------"
+    $tmp_accepted = ($upload | sort-Object -Property "Student ID" -Unique).Count
+    $tmp_rejected = ($rejected | sort-Object -Property "UPN Student" -Unique).Count
+    $tmp_unknown = ($unknown | sort-Object -Property "UPN Student" -Unique).Count
+    $tmp_total = ($spreadsheet.Group | sort-Object -Property "UPN Student" -Unique).Count
+    Write-Output "In accepted classes:`t $tmp_accepted"
+    Write-Output "In rejected classes:`t $tmp_rejected"
+    Write-Output "In unknown classes:`t`t $tmp_unknown"
+    Write-Output "In all classes:`t`t`t $tmp_total"
+}
+
+#Run any require PostProcess from config
+if (Get-Command 'PostProcess' -ErrorAction SilentlyContinue){
+    PostProcess
+}
